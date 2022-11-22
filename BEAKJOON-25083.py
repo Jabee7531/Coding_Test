559b3e64d9bbbf6aa6e4822dd390eb0f48cf1804
JaBee <JaBee7531@gmail.com>
2022-11-22 18:41:21 +0900

    feat : Add BEAKJOON-25083.py

---
+++ BEAKJOON-25083.py
@@ -0,0 +1,14 @@
+def solution():
+    print('         ,r\'"7')
+    print("r`-_   ,'  ,/")
+    print(" \. \". L_r'")
+    print("   `~\/")
+    print("      |")
+    print("      |")
+
+
+    return 0
+
+if __name__ == "__main__":
+    
+    result = solution()
