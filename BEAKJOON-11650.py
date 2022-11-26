2e83c32f9987892e5df3d6ee806636aee24df8b0
JaBee <JaBee7531@gmail.com>
2022-11-26 14:53:22 +0900

    feat : Add BEAKJOON-11650.py

--- BEAKJOON-11650.py
+++ BEAKJOON-11650.py
@@ -8,7 +8,7 @@ def solution():
         li.append(list(map(int, sys.stdin.readline().split())))
 
 
-    li.sort()
+    li.sort(key=lambda x : (x[1], x[0]))
 
     for i in range(n):
         print(li[i][0],li[i][1])
