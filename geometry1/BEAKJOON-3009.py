3a66f91bd79ba4a24f27fc35fa9182915476e40b
JaBee7531 <Jabee7531@gmail.com>
2022-12-10 16:45:04 +0900

    feat : Add BEAKJOON-3009.py

--- geometry1/BEAKJOON-3009.py
+++ geometry1/BEAKJOON-3009.py
@@ -2,14 +2,25 @@ def solution():
     import sys
 
     input_ = sys.stdin.readline
-
+        
     x1, y1 = map(int, input_().split())
     x2, y2 = map(int, input_().split())
     x3, y3 = map(int, input_().split())
 
-    
-    
+    if x1 == x2:
+        print(x3, end=" ")
+    elif x2 == x3:
+        print(x1, end=" ")
+    else:
+        print(x2, end=" ")
 
-    
+    if y1 == y2:
+        print(y3)
+    elif y2 == y3:
+        print(y1)
+    else:
+        print(y2)
+
+
 if __name__ == "__main__":
     solution()
