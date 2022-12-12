0c92b46c5ffa9a05b12f7d4d1c639142d62d0df6
JaBee7531 <Jabee7531@gmail.com>
2022-12-12 17:58:06 +0900

    feat : Add BEAKJOON-3053.py

---
+++ geometry1/BEAKJOON-3053.py
@@ -0,0 +1,13 @@
+def solution():
+    import sys
+    from math import pi
+    input_ = sys.stdin.readline
+
+    r = float(input_())
+
+    print(r*r*pi)
+    print(r*r*2)
+
+
+if __name__ == "__main__":
+    solution()
